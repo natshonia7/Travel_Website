d6bc4afc77c41f96166f6011e0f033cc81fea148
nikadolidze <nikadolidze@lemondo.com>
2023-06-07 00:38:44 +0400

    Create AboutUs.js

---
+++ src/components/AboutUs.js
@@ -0,0 +1,33 @@
+import './AboutUsStyles.css'
+
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div className="about-container">
+        <h1>Our History</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos ipsa dolorum nobis amet earum fuga ipsam corporis, natus consequuntur labore quas saepe doloribus nisi 
+            quos aspernatur quis tempora tempore! Explicabo.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, asperiores molestiae nihil perspiciatis culpa enim possimus numquam aliquid modi mollitia repellat perferendis dolor iure quis.
+             Possimus quos voluptatum odio quod?
+        </p>
+
+        <h1>Our Mission</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos ipsa dolorum nobis amet earum fuga ipsam corporis, natus consequuntur labore quas saepe doloribus nisi 
+            quos aspernatur quis tempora tempore! Explicabo.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, asperiores molestiae nihil perspiciatis culpa enim possimus numquam aliquid modi mollitia repellat perferendis dolor iure quis.
+             Possimus quos voluptatum odio quod?
+        </p>
+
+
+        <h1>Our Vision</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos ipsa dolorum nobis amet earum fuga ipsam corporis, natus consequuntur labore quas saepe doloribus nisi 
+            quos aspernatur quis tempora tempore! Explicabo.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, asperiores molestiae nihil perspiciatis culpa enim possimus numquam aliquid modi mollitia repellat perferendis dolor iure quis.
+             Possimus quos voluptatum odio quod?
+        </p>
+    </div>
+  )
+}
+
+export default AboutUs
