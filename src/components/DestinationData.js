a282eb947f5281bcca0c177bb6f9e65d73d20905
nikadolidze <nikadolidze@lemondo.com>
2023-06-07 00:37:47 +0400

    Create DestinationData.js

---
+++ src/components/DestinationData.js
@@ -0,0 +1,26 @@
+import {Component} from 'react'
+import './DestinationStyles.css'
+
+
+
+class DestinationData extends Component{
+    render(){
+       return(
+        <div className={this.props.className}>
+        <div className="des-text">
+            <h1>{this.props.heading}</h1>
+            <p>{this.props.text}</p>
+        </div>
+
+        <div className="image">
+            <img src={this.props.img1} alt="img" />
+            <img src={this.props.img2} alt="img" />
+
+        </div>
+    </div>
+       )
+    }
+}
+
+
+export default DestinationData;
